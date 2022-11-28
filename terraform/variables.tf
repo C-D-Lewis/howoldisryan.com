b29fb0aeafbf37d4870e4fcbc32a969f20128fd4
Chris Lewis <Bonsitm@gmail.com>
2022-11-28 21:11:45 +0000

    Deploy to AWS with GitHub actions

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name for all resources"
+  default     = "howoldisryan"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC to deploy into"
+  default     = "vpc-c3b70bb9"
+}
+
+variable "zone_id" {
+  type        = string
+  description = "Route53 zone ID"
+  default     = "Z09638222JXENL53LJQWF"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Site domain name, matching client_bucket"
+  default     = "howoldisryan.com"
+}
+
+variable "alt_domain_name" {
+  type        = string
+  description = "Alternate CNAME domain name, if any"
+  default     = "www.howoldisryan.com"
+}
+
+variable "certificate_arn" {
+  type        = string
+  description = "Certificate ARN in ACM"
+  default     = "arn:aws:acm:us-east-1:617929423658:certificate/5aa4f18e-821e-475d-b7d2-204ac661ef62"
+}
